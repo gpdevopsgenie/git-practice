0f21eae5ec85264dcde48456dbf88b2a15431a41
GP <gaoty.williams@gmail.com>
2023-02-01 22:55:37 -0600

    update sh file

--- server_inventory.sh
+++ server_inventory.sh
@@ -14,14 +14,21 @@ echo "checking the os version"
 sleep 2
 cat /etc/os-release
 echo "checking kernel version"
-sleep2
+sleep 2
 uname -r
 echo "checking the hardware"
 sleep 2
 lsblk
 echo "checking if system is 32 or 64 bits"
 sleep 2
-uname -r | awk -F_{print$2}'
+uname -r |awk -F_ '{print$2}'
 echo "checking the hardwares info"
 sleep 2
-lscpi
+lscpu
+
+# uname -r |awk -F. '{print$1"."$2}' this will print two fields at the same time  
+  #uname -r|awk -F_ '{print$2}'  this will print just the 64 of the uname -r command
+  #cat /etc/os-release |grep ^ID= |awk -F= '{print$2}'  the will print "centos"
+  #cat /etc/os-release |grep ^ID= |awk -F= '{print$2}'|awk -F'"' '{print$2}'  will print only centos
+  #cat /etc/os-release |awk -F= '{print$2}'|tail -5|head -1 this will print "centos-7"
+  #cat /etc/os-release |grep ^CENTOS_MANT* |awk -F= '{print$2}'|head -1
